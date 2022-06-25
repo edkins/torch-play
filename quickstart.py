361c017ef7a2524e9c85098a4d055ea4484f2c49
Giles Edkins <edkins@gmail.com>
2022-06-25 13:36:07 -0400

    Failed LeNet

--- quickstart.py
+++ quickstart.py
@@ -40,20 +40,25 @@ print(f"w = {w}, h = {h}")
 class NeuralNetwork(nn.Module):
     def __init__(self, w, h):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-                nn.Linear(w * h, 512),
-                nn.ReLU(),
-                nn.Linear(512, 512),
-                nn.ReLU(),
-                nn.Linear(512, 10),
-                nn.Softmax(dim=1),
+        self.lenet = nn.Sequential(
+                nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2),
+                nn.Sigmoid(),
+                nn.AvgPool2d(kernel_size=2, stride=2),
+                nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+                nn.Sigmoid(),
+                nn.AvgPool2d(kernel_size=2, stride=2),
+                nn.Flatten(),
+                nn.Linear(5*5*16, 120),
+                nn.Sigmoid(),
+                nn.Linear(120, 84),
+                nn.Sigmoid(),
+                nn.Linear(84, 10),
         )
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        logits = self.lenet(x)
+        return self.softmax(logits)
 
 model = NeuralNetwork(w,h).to(device)
 print(model)
