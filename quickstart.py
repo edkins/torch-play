336adb6e39e234d851b98147e23ce76a8e011734
Giles Edkins <edkins@gmail.com>
2022-06-25 11:53:43 -0400

    Draw graph of accuracy over epochs

--- quickstart.py
+++ quickstart.py
@@ -91,11 +91,21 @@ def test(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return correct
 
-epochs = 5
+epochs = 10
+correctness = []
 for t in range(epochs):
     print(f"Epoch {t+1}\n----------------------")
     train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    correct = test(test_dataloader, model, loss_fn)
+    correctness.append(correct)
+    plt.clf()
+    plt.plot(correctness)
+    plt.ylabel('accuracy')
+    plt.ylim((0, 1))
+    plt.xlabel('epoch')
+    plt.pause(0.1)
+plt.show()
 print("Done!")
 
