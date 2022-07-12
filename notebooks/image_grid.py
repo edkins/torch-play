5d6f0f177602c5bb4f673ad2e117c4f09276f05b
Giles Edkins <edkins@gmail.com>
2022-07-11 23:07:50 -0400

    Crazy colour scheme

--- notebooks/image_grid.py
+++ notebooks/image_grid.py
@@ -4,6 +4,41 @@ from matplotlib import pyplot as plt
 import torch
 from sklearn.decomposition import PCA, NMF
 
+_positive_colors = [
+    (1, 1, 1),    # white
+    (1, 0.5, 1),  # pink
+    (1, 0, 1),    # magenta
+    (1, 0, 0.5),  # raspberry
+    (1, 0, 0),    # red
+    (1, 0.5, 0),  # orange
+    (1, 1, 0),    # yellow
+    (0.7, 0.5, 0),# gold
+    (0, 0, 0),    # black
+]
+
+_negative_colors = [
+    (1, 1, 1),    # white
+    (0.5, 0.7, 1),# light blue
+    (0.0, 0.4, 1),# mid blue
+    (0, 0, 1),    # dark blue
+    (0, 0.5, 0.5),# teal
+    (0, 1, 1),    # cyan
+    (0, 1, 0),    # green
+    (0, 0.5, 0),  # dark green
+    (0.5, 0.5, 0),# olive
+]
+
+_maximum = 2 ** len(_positive_colors)
+
+_mappings = {
+    color: [(0.5 - (2**i)/_maximum, _negative_colors[i][ch], _negative_colors[i][ch]) for i in range(len(_positive_colors)-1,0,-1)] + [(0.5 if i==0 else 0.5 + (2**i)/_maximum, _positive_colors[i][ch], _positive_colors[i][ch]) for i in range(len(_positive_colors))]
+        for ch,color in enumerate(['red','green','blue'])
+}
+
+print(_mappings)
+
+cmap = mpl.colors.LinearSegmentedColormap("my_colormap", _mappings)
+
 class ImageGrid:
     def __init__(self, tensor: torch.Tensor):
         if len(tensor.size()) != 4:
@@ -33,25 +68,26 @@ class ImageGrid:
     def im_pixel_count(self) -> int:
         return self.tensor.size(2) * self.tensor.size(3)
 
-    def plot(self, figsize:tuple[int,int]=(6,6), title: str='', norm:str='all') -> None:
+    def plot(self, figsize:tuple[int,int]=(6,6), title: str='') -> None:
         mpl.rcParams['figure.figsize'] = figsize
-        if norm == 'all':
-            data = self.tensor.to('cpu').numpy()
-            normed_data = mpl.colors.SymLogNorm(1, 2)(data.reshape(-1)).reshape(data.shape)
-        elif norm == 'cols':
-            normed_data = self.tensor.to('cpu').numpy()
-            shape = (normed_data.shape[0],) + normed_data.shape[2:]
-            for c in range(self.ncols):
-                normed_data[:,c] = mpl.colors.SymLogNorm(1, 2)(normed_data[:,c].reshape(-1)).reshape(shape)
-        else:
-            raise ValueError(f'ImageGrid.plot norm must be "all" or "cols" not {norm}')
+        # if norm == 'all':
+        #     data = self.tensor.to('cpu').numpy()
+        #     normed_data = mpl.colors.SymLogNorm(1, 2)(data.reshape(-1)).reshape(data.shape)
+        # elif norm == 'cols':
+        #     normed_data = self.tensor.to('cpu').numpy()
+        #     shape = (normed_data.shape[0],) + normed_data.shape[2:]
+        #     for c in range(self.ncols):
+        #         normed_data[:,c] = mpl.colors.SymLogNorm(1, 2)(normed_data[:,c].reshape(-1)).reshape(shape)
+        # else:
+        #     raise ValueError(f'ImageGrid.plot norm must be "all" or "cols" not {norm}')
         for row in range(self.nrows):
             for col in range(self.ncols):
                 plt.subplot(self.nrows, self.ncols, row*self.ncols+col+1)
                 plt.xticks([])
                 plt.yticks([])
-                plt.imshow(normed_data[row,col], norm=mpl.colors.NoNorm(), cmap=mpl.cm.coolwarm)
+                plt.imshow(0.5 + self.tensor[row,col].to('cpu').numpy() / 8, norm=mpl.colors.NoNorm(), cmap=cmap)
         plt.suptitle(title)
+        #plt.colorbar()
         plt.show()
 
     def _to_column_matrix(self) -> torch.Tensor:
